60393fe2e9062fe0a0af1d1e94a4d98e78f156d3
Hengheng Zhang <imhmhm@gmail.com>
2020-09-24 15:42:00 -0500

    09-24-2020

---
+++ demo/demo.sh
@@ -0,0 +1,4 @@
+python demo.py \
+--config-file ../configs/COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml \
+--input c1s1_011276.jpg \
+--opts MODEL.WEIGHTS /home/hmhm/detectron2/model/mask_rcnn_R_50_FPN_3x.pkl
---
+++ projects/DensePose/example.sh
@@ -0,0 +1,7 @@
+python apply_net.py \
+show \
+configs/densepose_rcnn_R_50_FPN_s1x.yaml \
+model/densepose_rcnn_R_50_FPN_s1x.pkl \
+c1s1_011276.jpg \
+bbox,dp_segm \
+-v
